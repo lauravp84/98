d51acb7de08c945150db81095df5177f8ac8cc89
lauravp84 <145235659+lauravp84@users.noreply.github.com>
2024-01-25 13:56:48 -0300

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,25 @@
+<html>
+<body>
+
+    <script src="game_page.js"></script>
+   
+</body>
+</html>
+
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    actual_answer = parseInt(number1) * parseInt(number2);
+  }
+
+  question_number = "<h4>" + number1 + "x"+ number2 + "</h4>";
+
+  input_box = "<br>Resposta : <input type='text' id='input_check_box'>";
+
+  check_button="<br><br><button class='btn btn-info' onclick='check()'>Verificar</button>";
+
+  row = question_number + input_box + check_button
+
+  document.getElementById("number1").value = "";
+  document.getElementById("number2").value = "";
+  
